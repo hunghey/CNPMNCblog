498d31b4a11fd2c309dbbbda1a93be3913e8aba4
hung <nguyenlephuhung15@gmail.com>
2023-10-16 16:38:14 +0700

    BLOG-9 Create model post

---
+++ BlogDjango/bloghome/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+from datetime import datetime, date
+# from ckeditor.fields import RichTextField
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    header_img=models.ImageField(null=True, blank=True, upload_to="images/")
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    # body = RichTextField(blank=True, null=True)
+    body = models.TextField()
+    post_date = models.DateField(auto_now_add=True)
+    category = models.CharField(max_length=255, default='coding')
+    snippet = models.CharField(max_length=255)
+    likes = models.ManyToManyField(User, related_name='blog_posts',blank=True)
+
+    def total_likes(self):
+        return self.likes.count()
+    
+    def __str__(self):
+        return self.title + " | "+ str(self.author)
+    
+    def get_absolute_url(self):
+        # return reverse('article-detail', args=(str(self.id)))
+         return reverse('home')
