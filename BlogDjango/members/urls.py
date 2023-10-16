ddbcaed64553bfdb2bb8240ae40e0016a3560abe
Huỳnh Thị Yến Khoa <yenkhoamc@gmail.com>
2023-10-16 21:48:43 +0700

    BLOG-7 login

---
+++ BlogDjango/members/urls.py
@@ -0,0 +1,9 @@
+from .views import  ChangePasswordView
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+
+urlpatterns = [
+    path('password/', ChangePasswordView.as_view(template_name = 'registration/change_password.html')),
+    path('password_success', views.password_success, name = 'password_success'),
+]
