ec0acec07f9f256e2ca96bdfb5aed20eda3063df
Tran Ngoc Phuong Linh <tranngocphuonglinh308@gmail.com>
2023-10-18 21:21:10 +0700

    BLOG-17 Init Database

--- BlogDjango/bloghome/models.py
+++ BlogDjango/bloghome/models.py
@@ -2,9 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 from datetime import datetime, date
-# from ckeditor.fields import RichTextField
+from ckeditor.fields import RichTextField
 # Create your models here.
-
 class Category(models.Model):
     name = models.CharField(max_length=255)
     def __str__(self):
@@ -14,17 +13,29 @@ class Category(models.Model):
         # return reverse('article-detail', args=(str(self.id)))
          return reverse('home')
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, null= True,  on_delete=models.CASCADE)
+    bio = models.TextField()
+    profile_pic = models.ImageField(null=True, blank=True, upload_to="images/profile/")
+    instagram_url = models.CharField(max_length=255, null=True, blank=True)
+    facebook_url = models.CharField(max_length=255, null=True, blank=True)
+    github_url = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.user)
+    def get_absolute_url(self):
+         return reverse('home')
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
     header_img=models.ImageField(null=True, blank=True, upload_to="images/")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    # body = RichTextField(blank=True, null=True)
-    body = models.TextField()
+    body = RichTextField(blank=True, null=True)
+    # body = models.TextField()
     post_date = models.DateField(auto_now_add=True)
     category = models.CharField(max_length=255, default='coding')
     snippet = models.CharField(max_length=255)
-    likes = models.ManyToManyField(User, related_name='blog_posts',blank=True)
+    likes = models.ManyToManyField(User, related_name='blog_posts',blank=True, null=True)
 
     def total_likes(self):
         return self.likes.count()
@@ -35,3 +46,12 @@ class Post(models.Model):
     def get_absolute_url(self):
         # return reverse('article-detail', args=(str(self.id)))
          return reverse('home')
+    
+class Comment (models.Model):
+    post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    body = models.TextField(blank=True, null=True)
+    date_comment = models.DateTimeField (auto_now_add=True)
+
+    def __str__(self):
+        return '%s - %s'% (self.post.title, self.name)
