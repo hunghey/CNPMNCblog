deb022e9406e00263447f04e2e575fe39711296b
hung <nguyenlephuhung15@gmail.com>
2023-10-20 09:34:56 +0700

    BLOG_15 Update add_comment

--- BlogDjango/members/urls.py
+++ BlogDjango/members/urls.py
@@ -1,4 +1,4 @@
-from .views import  ChangePasswordView, EditProfilePageView, CreateProfilePageView
+from .views import  ChangePasswordView
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
@@ -6,7 +6,5 @@ from . import views
 urlpatterns = [
     path('password/', ChangePasswordView.as_view(template_name = 'registration/change_password.html')),
     path('password_success', views.password_success, name = 'password_success'),
-      path('<int:pk>/profile/', ShowProfilePageView.as_view(), name='show_profile_page'),
-    path('<int:pk>/edit_profile_page/', EditProfilePageView.as_view(), name='edit_profile_page'),
-    path('create_profile_page/', CreateProfilePageView.as_view(), name='create_profile_page'),
+
 ]
