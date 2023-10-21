2f805072a2dbf3cf6356862126f161ddca67bebe
0KHANG0 <khangnc.cnthongtin@gmail.com>
2023-10-21 22:41:20 +0700

    BLOG-14 add category

--- BlogDjango/bloghome/views.py
+++ BlogDjango/bloghome/views.py
@@ -1,10 +1,30 @@
-from django.shortcuts import render
-from .forms import PostForm, EditForm
+from django.shortcuts import render, get_object_or_404
+from .forms import PostForm, EditForm, AddCommentForm
 from django.http import HttpResponseRedirect
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import Post, Category
+from .models import Post, Category, Comment
 from django.urls import reverse_lazy, reverse
 
+
+# Create your views here.
+
+# def index(request):
+#     return render(request, 'pages/home.html')
+def contact(request):
+    return render(request, 'pages/contact.html')
+def error(request, *args, **kwargs):
+    return render(request,'pages/error.html')
+
+def LikeView(request, pk):
+    post = get_object_or_404(Post, id=request.POST.get('post_id'))
+    liked = False
+    if post.likes.filter(id=request.user.id).exists():
+        post.likes.remove(request.user)
+        liked=False
+    else:
+        post.likes.add(request.user)
+        liked = True
+    return HttpResponseRedirect(reverse('article-detail', args=[str(pk)]))
 
 
 class HomeView(ListView):
@@ -17,7 +37,28 @@ class HomeView(ListView):
         context = super(HomeView, self).get_context_data()
         context["cat_menu"] = cat_menu
         return context
-    
+
+
+class ArticleDetailView(DetailView):
+    model = Post
+    template_name = 'article_details.html'
+
+    def get_context_data(self, *args, **kwargs):
+        cat_menu = Category.objects.all()
+        context = super(ArticleDetailView, self).get_context_data()
+
+        stuff = get_object_or_404(Post, id=self.kwargs['pk'])
+        total_likes = stuff.total_likes()
+
+        liked= False
+        if stuff.likes.filter(id=self.request.user.id).exists():
+            liked=True
+
+        context["cat_menu"] = cat_menu
+        context["total_likes"] = total_likes
+        context["liked"] = liked
+        return context    
+
 # ---------------Post-----------------------
 class AddPostView(CreateView):
     model = Post
@@ -34,7 +75,6 @@ class DeletePostView(DeleteView):
     model = Post
     template_name = 'delete_post.html'
     success_url = reverse_lazy('home')
-
 # ---------------Category-----------------------
 class AddCategoryView(CreateView):
     model = Category
@@ -62,3 +102,5 @@ class AddCommentView(CreateView):
         return super().form_valid(form)
     
     success_url = reverse_lazy('home')
+
+    
