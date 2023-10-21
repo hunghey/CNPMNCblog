a406b94e8b19d15b5faaba6ed4e9158241e1d155
0KHANG0 <khangnc.cnthongtin@gmail.com>
2023-10-21 22:32:15 +0700

    BLOG-6 sign up

--- BlogDjango/members/urls.py
+++ BlogDjango/members/urls.py
@@ -1,4 +1,4 @@
-from .views import  ChangePasswordView
+from .views import  ChangePasswordView, UserRegisterView
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
     path('password/', ChangePasswordView.as_view(template_name = 'registration/change_password.html')),
     path('password_success', views.password_success, name = 'password_success'),
+    path('register/', UserRegisterView.as_view(), name = 'register'),
 
 ]
