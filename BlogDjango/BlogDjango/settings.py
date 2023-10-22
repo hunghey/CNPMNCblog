df58c79d0a7626b3dfbda8708845ca324ace8a05
Huỳnh Thị Yến Khoa <yenkhoamc@gmail.com>
2023-10-22 16:35:19 +0700

    update final blog

--- BlogDjango/BlogDjango/settings.py
+++ BlogDjango/BlogDjango/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'blog.urls'
+ROOT_URLCONF = 'BlogDjango.urls'
 
 TEMPLATES = [
     {
@@ -71,7 +71,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'blog.wsgi.application'
+WSGI_APPLICATION = 'BlogDjango.wsgi.application'
 
 
 # Database
